2a189d7e534207a881433d30e423c74f3602cc7a
younes-dro <dro66_8@hotmail.fr>
2019-07-26 03:25:46 +0100

    fixe the php open tag

--- page.php
+++ page.php
@@ -11,30 +11,34 @@
  *
  * @package dro_one_page_converter
  */
-
 get_header();
 ?>
+<div class="col-12">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+            <?php
+            while (have_posts()) :
+                the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+                get_template_part('template-parts/content', 'page');
 
-			get_template_part( 'template-parts/content', 'page' );
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+            endwhile; // End of the loop.
+            ?>
 
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+        </main><!-- #main -->
+    </div><!-- #primary -->
+</div>
+<div class="col-12">
+    <?php
+    get_sidebar();
+    ?>
+    <div/>
+    <?php
+    get_footer();
+    
