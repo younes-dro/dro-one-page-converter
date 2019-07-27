519ee14be4cbccfccdc347259f36868978581a3b
younes-dro <dro66_8@hotmail.fr>
2019-07-27 02:05:36 +0100

    Remove the sidebar footer

--- functions.php
+++ functions.php
@@ -119,15 +119,6 @@ function dro_one_page_converter_widgets_init() {
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ));
-    register_sidebar(array(
-        'name' => esc_html__('Sidebar Footer', 'dro-one-page-converter'),
-        'id' => 'sidebar-footer',
-        'description' => esc_html__('Add widgets here.', 'dro-one-page-converter'),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h2 class="widget-title">',
-        'after_title' => '</h2>',
-    ));
 }
 
 add_action('widgets_init', 'dro_one_page_converter_widgets_init');
