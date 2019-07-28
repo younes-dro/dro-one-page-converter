3285f85d0dab89b12864ecb35704129599865110
younes-dro <dro66_8@hotmail.fr>
2019-07-28 06:00:06 +0100

    styling the comments area

--- comments.php
+++ comments.php
@@ -32,21 +32,17 @@ if ( post_password_required() ) {
 			if ( '1' === $dro_one_page_converter_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'dro-one-page-converter' ),
-					'<span>' . get_the_title() . '</span>'
+					esc_html__( '1 Comment:', 'dro-one-page-converter' )
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $dro_one_page_converter_comment_count, 'comments title', 'dro-one-page-converter' ) ),
-					number_format_i18n( $dro_one_page_converter_comment_count ),
-					'<span>' . get_the_title() . '</span>'
+					esc_html( _nx( '%1$s Comment:', '%1$s Comments:', $dro_one_page_converter_comment_count, 'comments title', 'dro-one-page-converter' ) ),
+					number_format_i18n( $dro_one_page_converter_comment_count )
 				);
 			}
 			?>
 		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
 			<?php
