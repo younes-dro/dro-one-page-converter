e9631873a6d2f1976e51eba169617405e86b5250
younes-dro <dro66_8@hotmail.fr>
2019-07-28 08:52:42 +0100

    Add custom header for all template except the index page

---
+++ header-no-branding.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The header for all template except the index page 
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package dro_one_page_converter
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="https://gmpg.org/xfn/11">
+
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <div id="page" class="site">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 header-container">
+                        <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'dro-one-page-converter'); ?></a>
+                        <header id="masthead" class="site-header">
+                            <nav id="site-navigation" class="main-navigation" role="navigation">
+                                <?php
+                                wp_nav_menu(array(
+                                    'theme_location' => 'menu-1',
+                                    'menu_id' => 'primary',
+                                ));
+                                ?>
+                            </nav><!-- #site-navigation -->
+                            <?php the_custom_logo() ?>
+                        </header><!-- #masthead -->
+                    </div><!-- .col-12.header-container-->
+                </div><!-- .row -->
+                <div id="content" class="site-content row">
+
